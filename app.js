fd92e07f0b439b4f6eaf53526b180b03bffaa51e
risaza01 <risaza0102@gmail.com>
2025-06-16 21:15:18 -0500

    Escuchando en el puerto 80

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ server.on("request", async (req, res) => {
   }
 });
 
-server.listen(3000, () => {
-  console.log("Servidor corriendo en http://localhost:3000");
+server.listen(80, () => {
+  console.log("Servidor corriendo en http://localhost:80");
 });
